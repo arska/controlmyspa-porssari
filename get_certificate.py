bae567985aa6950f10ed42506411e488924a1f2d
Aarno Aukia <aarno.aukia@vshn.ch>
2024-06-08 14:13:40 +0200

    certifi trust store is cached, so the added chain certificate is only available on the second invocation

--- get_certificate.py
+++ get_certificate.py
@@ -1,13 +1,15 @@
-"""
-download the iot.controlmyspa.com intermediate TLS certificate
-during container build time
-"""
+"""download the iot.controlmyspa.com intermediate TLS certificate
+during container build time."""
 
 import controlmyspa
 import requests
 
 try:
     api = controlmyspa.ControlMySpa("dummy", "dummy")
+except requests.exceptions.SSLError:
+    # the certificate will be downloaded and added to the certifi trust store
+    # but it will only be available on the next invocation
+    pass
 except requests.exceptions.HTTPError:
     # catch the 401 Unauthorized error due to dummy credentials
     pass
